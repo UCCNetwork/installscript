5aeb58dc72f48981f6c4aaf76a98efc8b53d6a78
Fly <3713548+flyinghuman@users.noreply.github.com>
2019-07-08 09:43:44 +0200

    Update update-ucc.sh

--- update-ucc.sh
+++ update-ucc.sh
@@ -2,11 +2,11 @@
 
 TMP_FOLDER=$(mktemp -d)
 BINARY_LINK="https://github.com/UCCNetwork/ucc/releases/download/v2.2.0.0/UCC-2.2.0.0-Linux64bit.zip"
-DEFAULT_USER="$( pgrep -n '$DAEMON_BINARY' | xargs -r ps -o uname= -p )"
 DAEMON_BINARY="uccd"
 CLI_BINARY="ucc-cli"
 DAEMON_BINARY_FILE="/usr/local/bin/$DAEMON_BINARY"
 CLI_BINARY_FILE="/usr/local/bin/$CLI_BINARY"
+DEFAULT_USER="$( pgrep -n '$DAEMON_BINARY' | xargs -r ps -o uname= -p )"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
