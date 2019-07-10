3ff6e6545772dddb3d2663f6342cde6d48a01633
Fly <3713548+flyinghuman@users.noreply.github.com>
2019-07-10 10:39:09 +0200

    Update update-ucc.sh

--- update-ucc.sh
+++ update-ucc.sh
@@ -40,10 +40,12 @@ function checks()
     sleep 2
     echo -e "${RED}Searching for the user and data-directory ...${NC}"
     sleep 2
-    DATADIR=$(find /home -type d -name ".ucc")
+    DATADIRS=$(find /home -type d -name ".ucc")
+    DATADIR=$(echo "$DATADIRS" | head -1)
     if [ -d $DATADIR ]; then
       DEFAULT_USER=$(echo "$DATADIR" | rev | awk -F \/ '{print $2}' | rev)
       echo -e "${GREEN}Found the user: $DEFAULT_USER. We continue with the update.${NC}"
+      echo -e "${GREEN}Found the Data-Dir(s): $DATADIRS.${NC}"
     else
       echo -e "${RED}UCC seems not to be installed.${NC}"
       read -e -p "$(echo -e $YELLOW Should we fetch the install-script and install a new masternode? [Y/N] $NC)" ICHOICE
